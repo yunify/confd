c037ec680a685d4be2d44209a7e1cbeb875089fd
jolestar <jolestar@gmail.com>
2016-12-26 14:48:06 +0800

    support windows

---
+++ resource/template/util_windows.go
@@ -0,0 +1,31 @@
+package template
+
+import (
+	"crypto/md5"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+)
+
+// fileStat return a fileInfo describing the named file.
+func fileStat(name string) (fi fileInfo, err error) {
+	if isFileExist(name) {
+		f, err := os.Open(name)
+		if err != nil {
+			return fi, err
+		}
+		defer f.Close()
+		stats, _ := f.Stat()
+		// windows is not support file uid and gid
+		fi.Uid = 0
+		fi.Gid = 0
+		fi.Mode = stats.Mode()
+		h := md5.New()
+		io.Copy(h, f)
+		fi.Md5 = fmt.Sprintf("%x", h.Sum(nil))
+		return fi, nil
+	} else {
+		return fi, errors.New("File not found")
+	}
+}
